2e614b3c83e18cd486a1abe0a5f42772917cdfd8
shmuelrot <134505750+shmuelrot@users.noreply.github.com>
2023-11-14 12:01:44 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://ShmuelRoth:<password>@cluster0.gbn9blz.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Other database configuration code...
