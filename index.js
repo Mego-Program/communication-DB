c8acfa25eaadf87cdf9f484490c7e5685aee7c4f
Admin <s8498087@gmail.com>
2023-11-16 12:58:24 +0200

    basic mongoose db

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
+import mongoose from "mongoose";
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ShmuelRoth:mh881999@cluster0.gbn9blz.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
+const start = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const connection = await connectToDatabase();
+    // Your database-related code here
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // Close the MongoDB connection after all operations
+    mongoose.connection.close();
   }
-}
-run().catch(console.dir);
+};                 
+
+let connect = null;
+
+const connectToDatabase = async () => {
+  const userName = "ShmuelRoth"; // Make sure to wrap string values in quotes
+  const password = "mh881999"; // Wrap in quotes
+  const cluster = "@cluster0.gbn9blz.mongodb.net/?retryWrites=true&w=majority";
+  const dbName = "ToDo";
+
+  const connectionURI = `mongodb+srv://${userName}:${password}${cluster}${dbName}`;
+
+  try {
+    if (!connect) {
+      connect = await mongoose.connect(connectionURI
+      //   useNewUrlParser: true, // This option is no longer necessary in MongoDB driver v4.0.0 and above
+      //   useUnifiedTopology: true, // This option is no longer necessary in MongoDB driver v4.0.0 and above
+        );
+      console.log("Connected to the database");
+    }
+    // return connect;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+  
+  const db = mongoose.connection;
+ 
+ 
+  db.on("error", console.error.bind(console, "Connection error:"));
+  console.log("jkjk");
+  db.once("open", async () => {
+  console.log("Connected to MongoDB!");
+  
+    // Example Schema and Model
+    const todoSchema = new mongoose.Schema({
+      content: {
+        type: String,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    });
+
+    const Todo = mongoose.model("ToDo", todoSchema, "chat");
+
+    // Create and Save a Document
+    const newTodo = new Todo({
+      content: "Do something",
+    });
+
+    try {
+      await newTodo.save();
+      console.log("Todo saved to the database");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Close the MongoDB connection after all operations
+      mongoose.connection.close();
+    }
+  });
+};
+start();
